c84fc01db8709eeec9aefc9ff02fbb04a5c66c25
Rajendra Lungeli <linkwithjs@gmail.com>
2021-08-31 20:15:04 +0545

    initial commit reactapp

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import app from "./images/app.jpg";
+import android from "./images/android.jpg";
+import dm from "./images/dm.jpg";
+import software from "./images/software.jpeg";
+import js from "./images/js.jpeg";
+import nodejs from "./images/nodejs.png";
+
+const Sdata = [
+  {
+    imgsrc: app,
+    title: "Application",
+  },
+  {
+    imgsrc: android,
+    title: "Android",
+  },
+  {
+    imgsrc: dm,
+    title: "Digital Marketing",
+  },
+  {
+    imgsrc: software,
+    title: "Software",
+  },
+  {
+    imgsrc: js,
+    title: "JavaScript",
+  },
+  {
+    imgsrc: nodejs,
+    title: "NodeJs",
+  },
+];
+
+export default Sdata;
